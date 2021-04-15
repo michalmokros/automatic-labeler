f26956abe289ff1620ca733a59dce3e468c1927c
michalmokros <mokros.michal14@gmail.com>
2021-04-15 20:52:07 +0200

    fifth try

--- test.js
+++ test.js
@@ -2,3 +2,4 @@ console.log("Hello Labeler");
 console.log("Second try with node modules");
 console.log("Third try with good function name");
 console.log("Fourth try with load");
+console.log("Fifth try with load");
