2312d98b5733610b09116eeed4fb36b9f2886f6f
michalmokros <mokros.michal14@gmail.com>
2021-04-15 17:11:59 +0200

    Third try with goo function name

--- test.js
+++ test.js
@@ -1,2 +1,3 @@
-console.log('Hello Labeler');
-console.log('Second try with node modules');
+console.log("Hello Labeler");
+console.log("Second try with node modules");
+console.log("Third try with good function name");
