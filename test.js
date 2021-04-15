e8fbd0df6ff7e828c7c8ff895b0478363db27d36
michalmokros <mokros.michal14@gmail.com>
2021-04-15 21:01:08 +0200

    sixth try

--- test.js
+++ test.js
@@ -3,3 +3,4 @@ console.log("Second try with node modules");
 console.log("Third try with good function name");
 console.log("Fourth try with load");
 console.log("Fifth try with load");
+console.log("Sixth try with load");
